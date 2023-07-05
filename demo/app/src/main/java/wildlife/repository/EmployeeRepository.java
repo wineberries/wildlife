9da6e0b410f727f826928b598c8099789cc39999
wineberries <cwoldorf@alkugov.com>
2023-07-05 23:20:35 +0000

    teardown of tutorial app. add new app objects.

---
+++ demo/app/src/main/java/wildlife/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package wildlife.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import wildlife.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, Long>{
+
+}
---
+++ demo/app/src/test/java/wildlife/DemoApplicationTests.java
@@ -0,0 +1,14 @@
+package wildlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
